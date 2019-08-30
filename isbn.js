924f1e11dc77bc641056808f6c73c76c119c8929
Sura <sura.karnawi@tfgm.com>
2019-08-30 18:15:14 +0100

    adding isbn

--- isbn.js
+++ isbn.js
@@ -1,9 +1,34 @@
 "use strict";
 
-const callsSomething = () => {
+const isSomething = () => {
   return "something";
 };
 
+const isString = x => {
+  if (typeof x === "string" || x instanceof String) {
+    return true;
+  } else {
+    return false;
+  }
+};
+
+const stringLenCheck = x => {
+  x = String(x);
+  if (x.length != 10) {
+    return false;
+  }
+  return true;
+};
+
+const checkNumbers = x => {
+  let numbers = parseInt(x, 10);
+  //convert a string to an array of integers
+  return x;
+};
+
 module.exports = {
-  callsSomething
+  isSomething,
+  isString,
+  stringLenCheck,
+  checkNumbers
 };
--- isbn_test.js
+++ isbn_test.js
@@ -2,8 +2,22 @@
 const { expect } = require("chai");
 const sut = require("./isbn");
 
-describe("callsSomething", () => {
+describe("validateISBN()", () => {
   it("should return something", () => {
-    expect(sut.callsSomething()).to.equal("something");
+    expect(sut.isSomething()).to.equal("something");
+  });
+
+  it("should return a string", () => {
+    expect(sut.isString("test")).to.equal(true);
+  });
+
+  it("should check if ISBN has 10 digits", () => {
+    let x = 1234567899;
+    expect(sut.stringLenCheck(x)).to.equal(true);
+  });
+
+  it("should check numbers of ISBN", () => {
+    //convert string to an array of integers
+    //expect(sut.checkNumbers(x).to.equal("[1,2,3,4,5,6,7,8,9,9]"));
   });
 });
